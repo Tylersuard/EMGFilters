84dedd3cbf9728d06717663ee4ae269275a76596
Tylersuard <41713505+Tylersuard@users.noreply.github.com>
2018-07-25 19:33:19 -0700

    Variable should be sampleFreq and not sampleRate

--- examples/SimpleEMGFilters/SimpleEMGFilters.ino
+++ examples/SimpleEMGFilters/SimpleEMGFilters.ino
@@ -1,33 +1,33 @@
 /*
- * Copyright 2017, OYMotion Inc.
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
- * THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
- * DAMAGE.
- *
- */
+* Copyright 2017, OYMotion Inc.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* 1. Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in
+*    the documentation and/or other materials provided with the
+*    distribution.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+* "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+* LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+* FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+* COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+* BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+* OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+* AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+* THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+* DAMAGE.
+*
+*/
 
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "Arduino.h"
@@ -37,93 +37,74 @@
 
 #include "EMGFilters.h"
 
-#define _DEBUG      0
+#define TIMING_DEBUG 1
 
-#define SensorInputPin A0 // input pin number
-
-// Define the `CALIBRATE` macro as 1 to calibrate the baseline value
-// of input sEMG signals.
-//
-// After wiring the sEMG sensors to the Arduino board, wear the
-// sEMG sensors. Relax your muscles for a few seconds, you
-// will be able to see a series of squared sEMG signals values get printed on
-// your serial terminal. Choose the maximal one as the baseline by setting
-// the `baseline` variable.
-//
-// After calibriting, change the `CALIBRATE` macro to 0, and rebuild this
-// project. The `envelope`, which is the squared sEMG signal data, will be
-// printed to the serial line. The developer can plot it using the Arduino
-// SerialPlotter.
-//
-// Note:
-//      After calibration, Any squared value of sEMG sigal below the
-//      baseline will be treated as zero.
-//
-//      It is recommended that you do calibration every time you wear
-//      the sEMG sensor.
-#define CALIBRATE 1
-
-int baseline = 200;
+#define SensorInputPin A4 // input pin number
 
 EMGFilters myFilter;
+// discrete filters must works with fixed sample frequence
+// our emg filter only support "SAMPLE_FREQ_500HZ" or "SAMPLE_FREQ_1000HZ"
+// other sampleRate inputs will bypass all the EMG_FILTER
+int sampleFreq = SAMPLE_FREQ_1000HZ;
+// For countries where power transmission is at 50 Hz
+// For countries where power transmission is at 60 Hz, need to change to
+// "NOTCH_FREQ_60HZ"
+// our emg filter only support 50Hz and 60Hz input
+// other inputs will bypass all the EMG_FILTER
+int humFreq = NOTCH_FREQ_60HZ;
 
-// Set the input frequency.
-//
-// The filters work only with fixed sample frequency of
-// `SAMPLE_FREQ_500HZ` or `SAMPLE_FREQ_1000HZ`.
-// Inputs at other sample rates will bypass
-SAMPLE_FREQUENCY sampleRate = SAMPLE_FREQ_1000HZ;
+// Calibration:
+// put on the sensors, and release your muscles;
+// wait a few seconds, and select the max value as the throhold;
+// any value under throhold will be set to zero
+static int Throhold = 9;
 
-// Time interval for processing the input signal.
-unsigned long long interval = 1000000ul / sampleRate;
-
-// Set the frequency of power line hum to filter out.
-//
-// For countries with 60Hz power line, change to "NOTCH_FREQ_60HZ"
-NOTCH_FREQUENCY humFreq = NOTCH_FREQ_50HZ;
-
+unsigned long timeStamp;
+unsigned long timeBudget;
 
 void setup() {
     /* add setup code here */
-    myFilter.init(sampleRate, humFreq, true, true, true);
+    myFilter.init(SAMPLE_FREQ_1000HZ, NOTCH_FREQ_60HZ, true, true, true);
 
     // open serial
     Serial.begin(115200);
+
+    // setup for time cost measure
+    // using micros()
+    timeBudget = 1e6 / 1000;
+    // micros will overflow and auto return to zero every 70 minutes
 }
 
 void loop() {
-    // Note: `micros()` will overflow and reset every about 70 minutes.
-    unsigned long long timeStamp = micros();
+    /* add main program code here */
+    // In order to make sure the ADC sample frequence on arduino,
+    // the time cost should be measured each loop
+    /*------------start here-------------------*/
+    timeStamp = micros();
 
-    int data = analogRead(SensorInputPin);
+    int Value = analogRead(SensorInputPin);
 
     // filter processing
-    int dataAfterFilter = myFilter.update(data);
+    int DataAfterFilter = myFilter.update(Value);
 
-    // Get envelope by squaring the input
-    int envelope = sq(dataAfterFilter);
+    int envlope = sq(DataAfterFilter);
+    // any value under throhold will be set to zero
+    envlope = (envlope > Throhold) ? envlope : 0;
 
-    if (CALIBRATE) {
+    timeStamp = micros() - timeStamp;
+    if (TIMING_DEBUG) {
+        // Serial.print("Read Data: "); Serial.println(Value);
+        // Serial.print("Filtered Data: ");Serial.println(DataAfterFilter);
         Serial.print("Squared Data: ");
-        Serial.println(envelope);
-    }
-    else {
-        // Any value below the `baseline` value will be treated as zero
-        if (envelope < baseline) {
-            dataAfterFilter = 0;
-            envelope = 0;
-        }
-        // You may plot the data using Arduino SerialPlotter.
-        Serial.print(envelope);
+        Serial.println(envlope);
+        // Serial.print("Filters cost time: "); Serial.println(timeStamp);
+        // the filter cost average around 520 us
     }
 
-    // Usually, you should still have (interval - timeElapsed) to do other work.
-    // Otherwise, you would have to lower down the `sampleRate`.
-    unsigned long long timeElapsed = micros() - timeStamp;
-#if _DEBUG
-    Serial.print("Filters cost time: ");
-    Serial.println(timeElapsed);
-#else
-    delay((interval - timeElapsed) / 1000);
-#endif
+    /*------------end here---------------------*/
+    // if less than timeBudget, then you still have (timeBudget - timeStamp) to
+    // do your work
+    delayMicroseconds(1000);
+    // if more than timeBudget, the sample rate need to reduce to
+    // SAMPLE_FREQ_500HZ
 }
